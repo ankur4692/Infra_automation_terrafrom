c8c118f33e06bc440f95e85d84cd38e36917cca3
Ankur <47654709+ankur4692@users.noreply.github.com>
2022-11-06 23:53:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIA5QKJ7BKOIMGXJFS2"
+  secret_key = "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"
+}
+
 resource "aws_vpc" "VPC" {
   cidr_block           = var.vpcCIDR
   enable_dns_hostnames = true
